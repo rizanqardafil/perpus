935ea55c0e04a12f7c14d11206b1d168b98a7b28
Rizan Qardafil <rizanqardafil14@gmail.com>
2022-07-10 00:00:42 +0700

    Perpus

--- resources/views/admin-lte/footer.blade.php
+++ resources/views/admin-lte/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
     <div class="row justify-content-center">
-      <strong>Copyright &copy; Musyahya 2021</strong>
+        <strong>Copyright &copy; Sistem Informasi Perpustakaan </strong>
     </div>
 </footer>
